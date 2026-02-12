e6212c7c2f41d950e9ef04bab060a1180eaa20db
Ismael Monleón Rubio <ismael.monleon73@gmail.com>
2026-02-12 13:31:41 +0100

    change ports for deploy

--- src/main/java/cybereats/fpmislata/com/tiendaback/microservices/payment/PaymentMicroservice.java
+++
@@ -1,8 +0,0 @@
-package cybereats.fpmislata.com.tiendaback.microservices.payment;
-
-import cybereats.fpmislata.com.tiendaback.microservices.payment.model.PagoTarjeta;
-
-public interface PaymentMicroservice {
-    void payment(PagoTarjeta request);
-
-}
